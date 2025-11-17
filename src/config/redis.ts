c72622e70a2fde57a5eb86b999e8010874ad4d25
xeureka <teklemariameureka@gmail.com>
2025-11-17 22:40:21 +0300

    fix: fsome config files

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -17,29 +17,3 @@ export async function connectRedis(){
 
 export default client;
 
-/** Sorted Set
- * 
- * members are by default sorted from smallest to greatest score
- * 
- * for users with the same score by default they will gonna be sorted based on their names lexographic order
- * 
- * redis guarantees that softed sets is always sorted
- * there is no additional sorting required for the client
- * 
- * That saves CPU ccles in the client and also reduces the code complexity
- * 
- * their complexity is also on logarithmic time
- * 
- * they are ideas chocie of implenting real time, low latency leaderboard, priotry queue, and econday indexes
- * 
- * 
- * the operations include :- add,remove, increment and retrive members
- * their operations start with the letter 'Z'
- * 
- * 
- * ZADD <key> <score> <member>
- * 
- * Increment/decrement player steps
- * 
- */
-
